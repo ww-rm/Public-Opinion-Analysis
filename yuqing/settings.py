ef306fd48ab46e08661b86c305401f47d4de34c5
ww-rm <ww-rm@qq.com>
2021-06-15 14:31:55 +0800

    change to debug

--- yuqing/settings.py
+++ yuqing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'my^mmv*b0!%j)@)@gj17*beupiv*5n(qv0ts45@4t4p)@eql29'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
